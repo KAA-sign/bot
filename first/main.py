09998665e3572d908cc7759a064f5aa7d0bd669d
KAA-sign <q89308033587@gmail.com>
2022-02-14 16:31:46 +0300

    add_batton_title

--- first/main.py
+++ first/main.py
@@ -1,15 +1,45 @@
+import datetime
+from sre_constants import CALL
 from subprocess import Popen
 from subprocess import PIPE
 
 from telegram import Bot
 from telegram import Update
+from telegram import ParseMode
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
+from telegram.ext import CallbackQueryHandler
+
 
 from config import load_config
+
+
+# 'callback_data' - это то, что будет присылать бот при нажатии на каждую ктопку.
+# Каждый идентификатор должен быть уникальным
+CALLBACK_BUTTON1_LEFT = 'callback_button1_left'
+CALLBACK_BUTTON2_RIGTH = 'callback_button2_right'
+CALLBACK_BUTTON3_MORE = 'callback_button3_more'
+CALLBACK_BUTTON4_BACK = 'callback_button4_back'
+CALLBACK_BUTTON5_TIME = 'callback_button5_time'
+CALLBACK_BUTTON6_PRICE = 'callback_button6_price'
+CALLBACK_BUTTON7_PRICE = 'callback_button7_price'
+CALLBACK_BUTTON8_PRICE = 'callback_button8_price'
+
+TITLES = {
+    CALLBACK_BUTTON1_LEFT: 'Новое сообщение &#9993',
+    CALLBACK_BUTTON2_RIGTH: 'Отредактировать &#9998',
+    CALLBACK_BUTTON3_MORE: 'Ещё &#8230',
+    CALLBACK_BUTTON4_BACK: 'Назад &#8678',
+    CALLBACK_BUTTON5_TIME: 'Время &#9200',
+    CALLBACK_BUTTON6_PRICE: 'USD $',
+    CALLBACK_BUTTON7_PRICE: 'EUR &#8364',
+    CALLBACK_BUTTON8_PRICE: 'Brent &#128738',
+}
 
 
 def do_start(bot: Bot, update: Update):
